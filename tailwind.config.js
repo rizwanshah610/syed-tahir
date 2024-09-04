6b270274712ac0613ffb79f474bc3caae0557ba5
rizwanshah610 <rizwanshah610@gmail.com>
2024-09-04 14:46:01 +0500

    Header complete with mobile responsive

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.liquid',
+    './**/*.json',
+    './**/*.html',
+    // Add other paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
